4bc0570f3dd3806326d5c4f3651ca2dec789b7f6
Zach Starnes <zachstarnes@gmail.com>
2014-04-14 01:15:12 -0400

    Fixed bugs in Application.php

--- src/Umbrella/Foundation/Application.php
+++ src/Umbrella/Foundation/Application.php
@@ -23,28 +23,21 @@ class Application
      *
      * @var string
      */
-    private $param_one = null;
+    private $param1 = null;
 
     /**
      * Second parameter passed to the function (if exists)
      *
      * @var string
      */
-    private $param_two = null;
+    private $param2 = null;
 
     /**
      * Third parameter passed to the function (if exists)
      *
      * @var string
      */
-    private $param_three = null;
-
-    /**
-     * Database object from the parameters file
-     *
-     * @var Database Object
-     */
-    private $db = null;
+    private $param3 = null;
 
     /**
      * Paths defined in the paths file
@@ -68,20 +61,81 @@ class Application
      */
     public function bindPaths(array $paths)
     {
-        foreach($path as $key => $value)
+        foreach($paths as $key => $value)
         {
-            $this->paths["app.{$key}", realpath($value)];
+            $this->paths[$key] = realpath($value);
         }
     }
 
     /**
-     * Open a new database connection
+     * Gets the URL and splits it into usable parts
      *
-     * @param  array $params
+     * @return string
+     */
+    public function splitUrl()
+    {
+        $url = (isset($_GET['url']) ? $_GET['url'] : null);
+
+        if(isset($url))
+        {
+            $url = rtrim($url, '/');
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            $url = explode('/', $url);
+
+            $this->controller   = (isset($url[0]) ? $url[0] : null);
+            $this->action       = (isset($url[1]) ? $url[1] : null);
+            $this->param1       = (isset($url[2]) ? $url[2] : null);
+            $this->param2       = (isset($url[3]) ? $url[3] : null);
+            $this->param3       = (isset($url[4]) ? $url[4] : null);
+        }
+        else
+        {
+            //throw new Exception("The URL was not found. Please check your sent URL.");
+        }
+    }
+
+    /**
+     * Runs the application
+     *
      * @return void
      */
-    public function connectDb(array $params)
+    public function run()
     {
+        $this->splitUrl();
 
+        $controllerPath = $this->paths['src'] . '\Controllers\\' . strtolower($this->controller) . '.php';
+        if(file_exists($controllerPath))
+        {
+            require($controllerPath);
+            $this->controller = new $this->controller();
+
+            if(method_exists($this->controller, $this->action))
+            {
+                if(isset($this->param3))
+                {
+                    $this->controller->{$this->action}($this->param1, $this->param2, $this->param3);
+                }
+                else if(isset($this->param2))
+                {
+                    $this->controller->{$this->action}($this->param1, $this->param2);
+                }
+                else if(isset($this->param1))
+                {
+                    $this->controller->{$this->action}($this->param1);
+                }
+                else
+                {
+                    $this->controller->{$this->action}();
+                }
+            }
+            else
+            {
+                //throw new Exception('Controller ' . $this->controller . ' does not have any method named ' . $this->action . '.');
+            }
+        }
+        else
+        {
+            echo 'file does not exist';
+        }
     }
 }
