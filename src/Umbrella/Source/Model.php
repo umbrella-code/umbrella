66e6a1b70c9b2f0d1fc963a690c79365e2f1ea66
Zach Starnes <zachstarnes@gmail.com>
2014-04-29 09:01:13 -0400

    added doctrine/orm to composer.json

--- src/Umbrella/Source/Model.php
+++ src/Umbrella/Source/Model.php
@@ -1,8 +1,17 @@
 <?php
 
+//---------------------------------------------------------------------------
+// Model
+//---------------------------------------------------------------------------
+//
+// This is the main model for the application. This will give functionalities
+// like queries to all created models. All other models that are created
+// must extend this class.
+//
+
 namespace Umbrella\Source;
 
 class Model
 {
-    
+
 }
